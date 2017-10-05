52cdf7e86ebf479fc6b9756b0be15992625eae18
irrevaldy <irrevaldy@gmail.com>
2017-10-05 14:47:19 +0700

    no message

--- prototipe/php/dbconnect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$username = "id3069449_root";
-$password = "airwisata14";
-$host = "localhost";
-$database = "id3069449_systemicriskbi";
-
-$server = mysqli_connect($host, $username, $password, $database);
-?>
